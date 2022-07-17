bbcd0e36c8589e99c0f6b5010a6b17a78418467e
Hyper0x0 <neonhyper0x0@gmail.com>
2022-07-17 14:11:35 +0200

    NC02.00 - Official Release from NC02.00.003 Dev
    Dev commit ref. 6e94a44

--- NDCA_V2.sol
+++ NDCA_V2.sol
@@ -5,8 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NDCA_V1 is Ownable {
+interface NDCA {
+    function totalDCA() external view returns (uint256);
+    function pairListed(uint256 _dcaIndex) external view returns(address srcToken, uint256 srcDecimals, address destToken, uint256 destDecimals);
+}
+
+contract NDCA_V2 is Ownable {
     using SafeMath for uint256;
+    using SafeMath for uint;
     
     struct pairStruct{
         address srcToken;
@@ -21,7 +27,11 @@ contract NDCA_V1 is Ownable {
     struct userStruct{
         uint256 srcAmount;
         uint256 tau;
-        uint256 dcaTimeEx;
+        uint256 nextDcaTime;
+        uint256 lastDcaTimeOk;
+        uint256 destTokenEarned;
+        uint averageBuyPrice;
+        uint code;
         bool fundsTransfer;
     }
     //DCAs
@@ -35,16 +45,24 @@ contract NDCA_V1 is Ownable {
     uint256 private feePercent;
     uint256 private minTauLimit; //days
     uint256 private maxTauLimit; //days
-    uint256 private  minSrcAmount;
+    uint256 private minSrcAmount;
     bool private networkEnable;
 
     //Events
-    event DCASwap(address indexed _receiver, address _srcToken, address destToken, uint256 _destAmount, uint256 _timestamp, uint _status);
+    event DCASwap(address indexed _receiver, address _srcToken, address _destToken, uint256 _destAmount, uint _status, uint256 _timestamp);
     event GetFunds(address indexed _sender, address _srcToken, uint256 _srcAmount, uint256 _timestamp);
     event Refund(address indexed _receiver, address _srcToken, uint256 _srcAmount, uint256 _timestamp);
     event CreatedDCA(address indexed _sender, uint256 _pairN, uint256 _srcAmount, uint256 _timestamp);
     event DeletedDCA(address indexed _sender, uint256 _pairN, uint256 _timestamp);
 
+    /**
+     * @dev Throws if called by any account other than the router.
+     */
+    modifier onlyRouter() {
+        require(neonRouter != nullAddress, "NEON: Router not defined");
+        require(msg.sender == neonRouter, "NEON: Only router is allowed");
+        _;
+    }
     /*
     * Constructor
     * () will be defined the unit of measure
@@ -129,6 +147,23 @@ contract NDCA_V1 is Ownable {
     function setMinAmount(uint256 _value) external onlyOwner {
         minSrcAmount = _value;
     }
+    /* V1.1
+    * @dev download pair list from another NDCA
+    * () will be defined the unit of measure
+    * @param _oldNDCA address of the old NDCA where pair list will be downloaded
+    * @return bool successfully completed
+    */
+    function downloadPairList(address _oldNDCA) external onlyOwner returns(bool){
+        NDCA pairPool = NDCA(_oldNDCA);
+        require(networkEnable == false, "NEON: Network has to be disabled");
+        uint256 poolTotalPairs = pairPool.totalDCA();
+        uint256 i;
+        for(i=1; i<=poolTotalPairs; i++){
+            (address srcToken, uint256 srcDecimals, address destToken, uint256  destDecimals) = pairPool.pairListed(i);
+            listNewPair(srcToken, srcDecimals, destToken, destDecimals);
+        }
+        return true;
+    }
     /*
     * @dev List new pair ot token
     * () will be defined the unit of measure
@@ -138,7 +173,7 @@ contract NDCA_V1 is Ownable {
     * @param _destDecimals token decimals
     * @return bool successfully completed
     */
-    function listNewPair(address _srcToken, uint256 _srcDecimals, address _destToken, uint256 _destDecimals) external onlyOwner returns(bool) {
+    function listNewPair(address _srcToken, uint256 _srcDecimals, address _destToken, uint256 _destDecimals) public onlyOwner returns(bool) {
         require(_srcToken != nullAddress && _destToken != nullAddress, "NEON: nullAddress not allowed");
         require(_srcToken != _destToken, "NEON: Source & Destination token must be different");
         uint256 i;
@@ -187,13 +222,11 @@ contract NDCA_V1 is Ownable {
         dca.users[msg.sender].srcAmount = _srcTokenAmount;
         dca.users[msg.sender].tau = _tau;
         uint256 tauSeconds = _tau.mul(24*60*60);
-        dca.users[msg.sender].dcaTimeEx = block.timestamp.add(tauSeconds);
+        dca.users[msg.sender].nextDcaTime = block.timestamp.add(tauSeconds);
 
         dca.totalUsers = dca.totalUsers.add(1);
         dca.usersList[dca.totalUsers] = msg.sender;
 
-        
-        require(srcToken.approve(neonRouter, preApprovalAmount), "NEON: Approval amount error");
         emit CreatedDCA(msg.sender, _dcaIndex, _srcTokenAmount, block.timestamp);
     }
     /*
@@ -220,7 +253,11 @@ contract NDCA_V1 is Ownable {
         dca.totalUsers = dca.totalUsers.sub(1);
         dca.users[msg.sender].srcAmount = 0;
         dca.users[msg.sender].tau = 0;
-        dca.users[msg.sender].dcaTimeEx = 0;
+        dca.users[msg.sender].nextDcaTime = 0;
+        dca.users[msg.sender].lastDcaTimeOk = 0;
+        dca.users[msg.sender].destTokenEarned = 0;
+        dca.users[msg.sender].averageBuyPrice = 0;
+        dca.users[msg.sender].code = 0;
         dca.users[msg.sender].fundsTransfer = false;
         emit DeletedDCA(msg.sender, _dcaIndex, block.timestamp);
     }
@@ -231,28 +268,26 @@ contract NDCA_V1 is Ownable {
     * @param _userIndex user number to check/execute dca
     * @return bool state of execution
     */ 
-    function routerExecuteDCA(uint256 _dcaIndex, uint256 _userIndex) external returns (bool){
+    function routerExecuteDCA(uint256 _dcaIndex, uint256 _userIndex) external onlyRouter returns (bool){
         require(networkEnable == true, "NEON: Network disabled");
         require(_dcaIndex > 0, "NEON: DCA index must be > 0");
-        require(neonRouter != nullAddress, "NEON: Router not defined");
         require(_userIndex != 0, "NEON: User index must be > 0");
-
         require(_dcaIndex <= totalDCAs, "NEON: DCA index not listed");
-        require(msg.sender == neonRouter, "NEON: Only router is allowed");
 
         pairStruct storage dca = neonDCAs[_dcaIndex];
         require(_userIndex <= dca.totalUsers, "NEON: User index doesn't exist");
         IERC20 srcToken = IERC20(dca.srcToken);
         address currentUser = dca.usersList[_userIndex];
-        require(block.timestamp >= dca.users[currentUser].dcaTimeEx, "NEON: Execution not required yet");
+        require(block.timestamp >= dca.users[currentUser].nextDcaTime, "NEON: Execution not required yet");
         
         uint256 amount = dca.users[currentUser].srcAmount;
         require(srcToken.balanceOf(currentUser) >= amount, "NEON: Insufficient amount");
         require(srcToken.allowance(currentUser, address(this)) >= amount, "NEON: Insufficient approved token");
-        require(dca.users[currentUser].fundsTransfer == false, "NEON: Funds already claimed");
-        dca.users[currentUser].fundsTransfer = true;
-        require(srcToken.transferFrom(currentUser, neonRouter, amount), "NEON: Funds already claimed");
-        emit GetFunds(currentUser, dca.srcToken, amount, block.timestamp);
+        if(dca.users[currentUser].fundsTransfer == false){
+            dca.users[currentUser].fundsTransfer = true;
+            require(srcToken.transferFrom(currentUser, neonRouter, amount), "NEON: Funds transfer error");
+            emit GetFunds(currentUser, dca.srcToken, amount, block.timestamp);
+        }
         return true;
     }
     /*
@@ -261,36 +296,44 @@ contract NDCA_V1 is Ownable {
     * @param _dcaIndex DCA where router has executed
     * @param _userIndex user dca executed
     * @param _destTokenAmount amount user will recieve
-    * @param _resultDCA integer to trace the DCA state
+    * @param _code integer to trace the DCA state of execution
+    * @param _unitaryPrice unit purchase
     * @return bool state of execution
     */
-    function routerResultDCA(uint256 _dcaIndex, uint256 _userIndex, uint256 _destTokenAmount, uint _resultDCA) external returns (bool) {
+    function routerResultDCA(uint256 _dcaIndex, uint256 _userIndex, uint256 _destTokenAmount, uint _code, uint _unitaryPrice) external onlyRouter returns (bool) {
         require(networkEnable == true, "NEON: Network disabled");
         require(_dcaIndex > 0, "NEON: DCA index must be > 0");
-        require(neonRouter != nullAddress, "NEON: Router not defined");
         require(_userIndex != 0, "NEON: User index must be > 0");
-
         require(_dcaIndex <= totalDCAs, "NEON: DCA index not listed");
-        require(msg.sender == neonRouter, "NEON: Only router is allowed");
         
         pairStruct storage dca = neonDCAs[_dcaIndex];
         require(_userIndex <= dca.totalUsers, "NEON: User index doesn't exist");
         IERC20 srcToken = IERC20(dca.srcToken);
         address currentUser = dca.usersList[_userIndex];
-        require(block.timestamp >= dca.users[currentUser].dcaTimeEx, "NEON: Execution not required yet");
+        require(block.timestamp >= dca.users[currentUser].nextDcaTime, "NEON: Execution not required yet");
         require(dca.users[currentUser].fundsTransfer == true, "NEON: Funds not claimed");
         uint256 amount = dca.users[currentUser].srcAmount;
         uint256 tau = dca.users[currentUser].tau;
         uint256 tauSeconds = tau.mul(24*60*60);
-        dca.users[currentUser].dcaTimeEx = block.timestamp.add(tauSeconds);
-        if(_resultDCA != 200){
+        dca.users[currentUser].nextDcaTime = dca.users[currentUser].nextDcaTime.add(tauSeconds);
+        dca.users[currentUser].code = _code;
+        if(_code == 200){
             dca.users[currentUser].fundsTransfer = false;
-            srcToken.transferFrom(neonRouter, currentUser, amount);
-            emit Refund(currentUser, dca.srcToken, amount, block.timestamp);
+            dca.users[currentUser].lastDcaTimeOk = block.timestamp;
+            dca.users[currentUser].destTokenEarned = dca.users[currentUser].destTokenEarned.add(_destTokenAmount);
+            if(dca.users[currentUser].averageBuyPrice == 0){//only first time
+                dca.users[currentUser].averageBuyPrice = _unitaryPrice;
+            }else{
+                dca.users[currentUser].averageBuyPrice = dca.users[currentUser].averageBuyPrice.add(_unitaryPrice).sub(2);//Average
+            }
         }else{
-            dca.users[currentUser].fundsTransfer = false;
+            if(dca.users[currentUser].fundsTransfer == true){
+                dca.users[currentUser].fundsTransfer = false;
+                require(srcToken.transferFrom(neonRouter, currentUser, amount), "NEON: Refunds transfer error");
+                emit Refund(currentUser, dca.srcToken, amount, block.timestamp);
+            }
         }
-        emit DCASwap(currentUser, dca.srcToken, dca.destToken, _destTokenAmount, _resultDCA, block.timestamp);
+        emit DCASwap(currentUser, dca.srcToken, dca.destToken, _destTokenAmount, _code, block.timestamp);
         return true;
     }
     /*
@@ -319,6 +362,22 @@ contract NDCA_V1 is Ownable {
         return neonDCAs[_dcaIndex].totalUsers;
     }
     /*
+    * @view Check if is router
+    * () will be defined the unit of measure
+    * @return true if the msg.sender is the router
+    */
+    function isRouter() external view returns(bool) {
+        return (msg.sender == neonRouter);
+    }
+    /*
+    * @view Check if is swapper
+    * () will be defined the unit of measure
+    * @return true if the msg.sender is the swapper
+    */
+    function isSwapper() external view returns(bool) {
+        return (msg.sender == swapper);
+    }
+    /*
     * @view token listed address
     * () will be defined the unit of measure
     * @param _dcaIndex DCA number
@@ -338,14 +397,14 @@ contract NDCA_V1 is Ownable {
     * @param _userIndex User number
     * @return bool enable execute
     */
-    function routerCheckDCA(uint256 _dcaIndex, uint256 _userIndex) external view returns(bool) {
+    function routerCheckDCA(uint256 _dcaIndex, uint256 _userIndex) external view onlyRouter returns(bool) {
         pairStruct storage dca = neonDCAs[_dcaIndex];
         address currentUser = dca.usersList[_userIndex];
         require(currentUser != nullAddress, "NEON: User doesn't exist");
         require(dca.users[currentUser].srcAmount > 0, "NEON: Invalid amount");
-        require(dca.users[currentUser].dcaTimeEx > 0, "NEON: Invalid execute time");
-        uint256 dcaTimeEx = dca.users[currentUser].dcaTimeEx;
-        return block.timestamp >= dcaTimeEx;
+        require(dca.users[currentUser].nextDcaTime > 0, "NEON: Invalid execute time");
+        uint256 nextDcaTime = dca.users[currentUser].nextDcaTime;
+        return block.timestamp >= nextDcaTime;
     }
     /*
     * @view Check user available amount
@@ -354,12 +413,12 @@ contract NDCA_V1 is Ownable {
     * @param _userIndex User number
     * @return bool enable execute
     */
-    function routerCheckAmount(uint256 _dcaIndex, uint256 _userIndex) external view returns(bool) {
+    function routerCheckAmount(uint256 _dcaIndex, uint256 _userIndex) external view onlyRouter returns(bool) {
         pairStruct storage dca = neonDCAs[_dcaIndex];
         address currentUser = dca.usersList[_userIndex];
         require(currentUser != nullAddress, "NEON: User doesn't exist");
         require(dca.users[currentUser].srcAmount > 0, "NEON: Invalid amount");
-        require(dca.users[currentUser].dcaTimeEx > 0, "NEON: Invalid execute time");
+        require(dca.users[currentUser].nextDcaTime > 0, "NEON: Invalid execute time");
         IERC20 srcToken = IERC20(dca.srcToken);
         return srcToken.balanceOf(currentUser) >= dca.users[currentUser].srcAmount;
     }
@@ -375,7 +434,15 @@ contract NDCA_V1 is Ownable {
     * @return reciever address user for DCA
     * @return srcTokenAmount amount to be swap
     */
-    function routerUserInfo(uint256 _dcaIndex, uint256 _userIndex) external view returns(address srcToken, uint256 srcDecimals, address destToken, uint256 destDecimals, address reciever, uint256 srcTokenAmount) {
+    function routerUserInfo(uint256 _dcaIndex, uint256 _userIndex) external view onlyRouter returns(
+        address srcToken,
+        uint256 srcDecimals,
+        address destToken,
+        uint256 destDecimals,
+        address reciever,
+        uint256 srcTokenAmount
+    )
+    {
         pairStruct storage dca = neonDCAs[_dcaIndex];
         address currentUser = dca.usersList[_userIndex];
         uint256 feeAmount = dca.users[currentUser].srcAmount.div(100).mul(feePercent);
@@ -394,13 +461,33 @@ contract NDCA_V1 is Ownable {
     * @return dcaActive DCA active
     * @return srcTokenAmount Amount invested for each DCA
     * @return tau frequency of execution
+    * @return nextDcaTime time next DCA execution
+    * @return lastDcaTimeOk time last DCA executed with success
+    * @return destTokenEarned total token bought since DCA creation
+    * @return averageBuyPrice unit purchase average
+    * @return code code of last execution DCA
     */
-    function dashboardUser(uint256 _dcaIndex) external view returns(bool dcaActive, uint256 srcTokenAmount, uint256 tau) {
+    function dashboardUser(uint256 _dcaIndex) external view returns(
+        bool dcaActive,
+        uint256 srcTokenAmount,
+        uint256 tau,
+        uint256 nextDcaTime,
+        uint256 lastDcaTimeOk,
+        uint256 destTokenEarned,
+        uint averageBuyPrice,
+        uint code
+    ) 
+    {
         pairStruct storage dca = neonDCAs[_dcaIndex];
         if(dca.users[msg.sender].srcAmount > 0){
+            dcaActive = true;
             srcTokenAmount = dca.users[msg.sender].srcAmount;
             tau = dca.users[msg.sender].tau;
-            dcaActive = true;
+            nextDcaTime = dca.users[msg.sender].nextDcaTime;
+            lastDcaTimeOk = dca.users[msg.sender].lastDcaTimeOk;
+            destTokenEarned = dca.users[msg.sender].destTokenEarned;
+            averageBuyPrice = dca.users[msg.sender].averageBuyPrice;
+            code = dca.users[msg.sender].code;
         }else{
             dcaActive = false;
         }
